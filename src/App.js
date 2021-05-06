e9f3ebcff567a2124501fc24e3a4ae347cdff3e9
Claire <claire.cy.t@gmail.com>
2021-05-05 23:01:56 -0400

    Add TodoForm, TodoItemList and TodoItem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { hot } from 'react-hot-loader';
+import TodoList from './todos/TodoList';
 
 const App = () => (
   <div className="App">
-    <h1>Hello</h1>
+    <TodoList />
   </div>
 );
 
-export default App;
+export default hot(module)(App);
