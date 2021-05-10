b2cb78a00808f409024ef25a0cc615097c4eef50
Claire <claire.cy.t@gmail.com>
2021-05-10 15:56:29 -0400

    Add thunks

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -4,11 +4,12 @@ import './TodoList.css';
 import NewTodoForm from './NewTodoForm';
 import { removeTodo, markTodoAsCompleted } from './actions';
 import { connect } from 'react-redux';
+import { displayAlert } from './thunks';
 
-const TodoList = ({ todos = [{ text: 'Hello' }], onRemovePressed, onCompletedPressed }) => (
+const TodoList = ({ todos = [{ text: 'Hello' }], onRemovePressed, onCompletedPressed, onDisplayAlertClicked }) => (
   <div className="list-wrapper">
     <NewTodoForm />
-    {todos.map(todo => <TodoListItem key={todo.text} todo={todo} onRemovePressed={onRemovePressed} onCompletedPressed={onCompletedPressed} />)}
+    {todos.map(todo => <TodoListItem key={todo.text} todo={todo} onRemovePressed={onRemovePressed} onCompletedPressed={onDisplayAlertClicked} />)}
   </div>
 )
 
@@ -16,5 +17,6 @@ const mapStateToProps = state => ({ todos: state.todos, });
 const mapDispatchToProps = dispatch => ({
   onRemovePressed: text => dispatch(removeTodo(text)),
   onCompletedPressed: text => dispatch(markTodoAsCompleted(text)),
+  onDisplayAlertClicked: () => dispatch(displayAlert()),
 });
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
