bb85f673a9020d597f95c05bfbd2f4fdf58afe7c
Claire <claire.cy.t@gmail.com>
2021-05-10 10:54:15 -0400

    add complete todo reducer

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -2,16 +2,19 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.css';
 import NewTodoForm from './NewTodoForm';
-import { removeTodo } from './actions';
+import { removeTodo, markTodoAsCompleted } from './actions';
 import { connect } from 'react-redux';
 
-const TodoList = ({ todos = [{ text: 'Hello' }], onRemovePressed }) => (
+const TodoList = ({ todos = [{ text: 'Hello' }], onRemovePressed, onCompletedPressed }) => (
   <div className="list-wrapper">
     <NewTodoForm />
-    {todos.map(todo => <TodoListItem key={todo.text} todo={todo} onRemovePressed={onRemovePressed} />)}
+    {todos.map(todo => <TodoListItem key={todo.text} todo={todo} onRemovePressed={onRemovePressed} onCompletedPressed={onCompletedPressed} />)}
   </div>
 )
 
 const mapStateToProps = state => ({ todos: state.todos, });
-const mapDispatchToProps = dispatch => ({ onRemovePressed: text => dispatch(removeTodo(text)), });
+const mapDispatchToProps = dispatch => ({
+  onRemovePressed: text => dispatch(removeTodo(text)),
+  onCompletedPressed: text => dispatch(markTodoAsCompleted(text)),
+});
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
