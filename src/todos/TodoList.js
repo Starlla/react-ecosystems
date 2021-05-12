743089cd7ef7129a999438b19af88a69018c3506
Claire <claire.cy.t@gmail.com>
2021-05-11 21:32:16 -0400

    connect thunks and load todos from server

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TodoListItem from './TodoListItem';
+import { loadTodos } from './thunks';
 import './TodoList.css';
 import NewTodoForm from './NewTodoForm';
 import { removeTodo, markTodoAsCompleted } from './actions';
 import { connect } from 'react-redux';
 import { displayAlert } from './thunks';
+import { isLoading } from './reducers';
 
-const TodoList = ({ todos = [{ text: 'Hello' }], onRemovePressed, onCompletedPressed, onDisplayAlertClicked }) => (
-  <div className="list-wrapper">
-    <NewTodoForm />
-    {todos.map(todo => <TodoListItem key={todo.text} todo={todo} onRemovePressed={onRemovePressed} onCompletedPressed={onDisplayAlertClicked} />)}
-  </div>
-)
+const TodoList = ({ todos = [], isLoading, onRemovePressed, onCompletedPressed, startLoadingTodos }) => {
+  useEffect(() => {
+    startLoadingTodos();
+  }, [])
+  const loadingMessage = <div>Loading todos...</div>;
+  const content = (
+    <div className="list-wrapper">
+      <NewTodoForm />
+      {todos.map(todo => <TodoListItem key={todo.text} todo={todo} onRemovePressed={onRemovePressed} onCompletedPressed={onCompletedPressed} />)}
+    </div>
+  )
+  return isLoading ? loadingMessage : content;
+}
 
-const mapStateToProps = state => ({ todos: state.todos, });
+const mapStateToProps = state => ({ todos: state.todos, isLoading: state.isLoading });
 const mapDispatchToProps = dispatch => ({
   onRemovePressed: text => dispatch(removeTodo(text)),
   onCompletedPressed: text => dispatch(markTodoAsCompleted(text)),
-  onDisplayAlertClicked: () => dispatch(displayAlert()),
+  startLoadingTodos: () => dispatch(loadTodos()),
 });
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
