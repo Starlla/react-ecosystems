cf755f1a55fe9631e6b8318b8d157d650124b89a
Claire <claire.cy.t@gmail.com>
2021-05-13 08:31:13 -0400

    Seperate complete and incomplate using reselect

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -4,9 +4,9 @@ import { loadTodos, removeTodoRequest, updateTodoRequest } from './thunks';
 import './TodoList.css';
 import NewTodoForm from './NewTodoForm';
 import { connect } from 'react-redux';
-import { getTodos, getTodosLoading } from './selectors';
+import { getTodos, getTodosLoading, getCompleteTodos, getIncompleteTodos } from './selectors';
 
-const TodoList = ({ todos = [], isLoading, onRemovePressed, onCompletedPressed, startLoadingTodos }) => {
+const TodoList = ({ completedTodos = [], incompletedTodos = [], isLoading, onRemovePressed, onCompletedPressed, startLoadingTodos }) => {
   useEffect(() => {
     startLoadingTodos();
   }, [])
@@ -14,13 +14,16 @@ const TodoList = ({ todos = [], isLoading, onRemovePressed, onCompletedPressed, 
   const content = (
     <div className="list-wrapper">
       <NewTodoForm />
-      {todos.map(todo => <TodoListItem key={todo.id} todo={todo} onRemovePressed={onRemovePressed} onCompletedPressed={onCompletedPressed} />)}
+      <h3>Incomplete:</h3>
+      {incompletedTodos.map(todo => <TodoListItem key={todo.id} todo={todo} onRemovePressed={onRemovePressed} onCompletedPressed={onCompletedPressed} />)}
+      <h3>Complete:</h3>
+      {completedTodos.map(todo => <TodoListItem key={todo.id} todo={todo} onRemovePressed={onRemovePressed} onCompletedPressed={onCompletedPressed} />)}
     </div>
   )
   return isLoading ? loadingMessage : content;
 }
 
-const mapStateToProps = state => ({ todos: getTodos(state), isLoading: getTodosLoading(state) });
+const mapStateToProps = state => ({ completedTodos: getCompleteTodos(state), incompletedTodos: getIncompleteTodos(state), isLoading: getTodosLoading(state) });
 const mapDispatchToProps = dispatch => ({
   onRemovePressed: id => dispatch(removeTodoRequest(id)),
   onCompletedPressed: id => dispatch(updateTodoRequest(id)),
